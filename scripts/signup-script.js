9909e4f92d72e0426d75591f75454a2a737e51bb
BegottenSon <danielbrown2d@gmail.com>
2021-06-29 09:24:06 -0400

    script for signup redirect

--- scripts/signup-script.js
+++ scripts/signup-script.js
@@ -2,11 +2,15 @@ const buySection = document.getElementById("buy-section");
 const dripForm = document.getElementById("drip-ef-117453377");
 const dripButton = document.getElementById("drip-button");
 
-buySection.style.visibility = "hidden";
 
 function showBuySection() {
-    buySection.style.visibility = "visible";
-    dripForm.style.display = "none";
+    if (window.location.href === "http://127.0.0.1:59751/reservation.html#buy-section") {
+        buySection.style.visibility = "visible";
+        dripForm.style.display = "none";
+    }else {
+        buySection.style.visibility = "hidden";
+        dripForm.style.display = "block"
+    }
 }
 
-dripButton.addEventListener('click', showBuySection);
+showBuySection()
